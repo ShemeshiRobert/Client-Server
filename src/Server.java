f8cad3f6cfceaa96e1fd1cf942cbabde835abc3a
sh744 <robertshemeshi@gmail.com>
2025-04-02 14:29:09 +0530

    Implemented Server minus the ClientHandler Class

---
+++ src/Server.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private ServerSocket serverSocket;
+    private static final int PORT = 3030;
+
+    public Server(){
+        try{
+            serverSocket = new ServerSocket(PORT);
+            acceptConnections();
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void acceptConnections() {
+        while(true){
+            try{
+                Socket clientSocket = serverSocket.accept();
+                if(clientSocket.isConnected()){
+                    ClientHandler clientHandler = new ClientHandler(clientSocket);
+                    new Thread(clientHandler).start();
+                }
+            } catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
